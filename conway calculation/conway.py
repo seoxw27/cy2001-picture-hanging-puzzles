57fffebd6a08a547e92cff03b4b118810124df9a
sarah <seoxw27@gmail.com>
2026-03-17 09:56:57 +0800

    17/3 0956

--- conway calculation/conway.py
+++ conway calculation/conway.py
@@ -3,14 +3,13 @@
 from sage.all import *
 
 # word for braid
-word = [2, 1, 1, 2, -1, 1] # change this obviously
+word = [1, 1] # change this obviously
 
 # counts number of strands, based on word
-k = max(abs(i) for i in word) + 1
+k = int(max(abs(i) for i in word) + 1)
 
 B = BraidGroup(k)
-b = B(word)
-L = Link(b)
+L = Link(B(word))
 
 # print("strands:", strands)
 print("Conway polynomial:", L.conway_polynomial())
