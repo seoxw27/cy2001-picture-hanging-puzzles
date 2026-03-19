a8f719ee54903079fe5864a1f4f1654948d19071
sarah <seoxw27@gmail.com>
2026-03-19 20:54:56 +0800

    19/3 2054

--- conway calculation/conway.py
+++ conway calculation/conway.py
@@ -1,17 +1,6 @@
-# wsl; conda activate sage
-
 from sage.all import *
 from sage.plot import *
-
-# word for braid
-braids = {
-    "2 original": [1, 1, -1, -1, 2, 2],
-    "2 transformed": [1, 1, -2, -2, -1, -1, 2, 2],
-    "3 original": [1, 1, -1, -1, 2, 2, -2, -2, 3, 3],
-    "3 transformed": [1, 1, -1, -1, 2, 2, -3, -3, -2, -2, 3, 3],
-    "4 original": [1, 1, -1, -1, 2, 2, -2, -2, 3, 3, -3, -3, 4, 4],
-    "4 transformed": [1, 1, -1, -1, 2, 2, -2, -2, 3, 3, -4, -4, -3, -3, 4, 4]
-}
+from braids import braids
 
 for name, word in braids.items():
     print(name, ":")
@@ -21,7 +10,7 @@ for name, word in braids.items():
     B = BraidGroup(k)
     b = B(word)
     print("Number of strands:" ,b.components_in_closure())
-    B(word).plot().save(f"{name}_braid.png")
+    B(word).plot(orientation = "top-bottom").save(f"{name}_braid.png")
 
     L = Link(b)
     print("Number of components",L.number_of_components())
