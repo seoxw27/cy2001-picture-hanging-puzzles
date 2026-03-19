e1a70762f659cd684acc578f15cab2b1d2e6d612
sarah <seoxw27@gmail.com>
2026-03-19 12:23:10 +0800

    19/3 1223

--- conway calculation/conway.py
+++ conway calculation/conway.py
@@ -1,6 +1,7 @@
 # wsl; conda activate sage
 
 from sage.all import *
+from sage.plot import *
 
 # word for braid
 word = [1, 1] # change this obviously
@@ -12,4 +13,8 @@ B = BraidGroup(k)
 L = Link(B(word))
 
 # print("strands:", strands)
+print(B(word).components_in_closure())
+print(L.number_of_components())
 print("Conway polynomial:", L.conway_polynomial())
+p = L.plot()
+p.save("link.png")
