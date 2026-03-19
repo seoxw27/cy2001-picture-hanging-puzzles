bf15a821e442fb0793e58b54cb43ddcab9799c77
sarah <seoxw27@gmail.com>
2026-03-19 13:00:40 +0800

    19/3 1300

--- conway calculation/conway.py
+++ conway calculation/conway.py
@@ -4,17 +4,20 @@ from sage.all import *
 from sage.plot import *
 
 # word for braid
-word = [1, 1] # change this obviously
+word = [1, 1, -1, -1, 2, 2, -3, -3, -2, -2, 3, 3] # change this obviously
+
+# word = [1, 1, -1, -1, 2, 2, -2, -2, 3, 3]
 
 # counts number of strands, based on word
 k = int(max(abs(i) for i in word) + 1)
 
 B = BraidGroup(k)
-L = Link(B(word))
+b = B(word)
+print("Number of strands:" ,b.components_in_closure())
+B(word).plot().save("braid.png")
 
-# print("strands:", strands)
-print(B(word).components_in_closure())
-print(L.number_of_components())
+L = Link(b)
+print("Number of components",L.number_of_components())
+L.plot().save("link.png")
+
 print("Conway polynomial:", L.conway_polynomial())
-p = L.plot()
-p.save("link.png")
